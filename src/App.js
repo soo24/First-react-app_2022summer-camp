ad9f2e672de78dff4efffec912eaddd3b6349a23
soo <soo@sooui-MacBookPro.local>
2022-07-11 17:47:24 +0900

    07/11 1.First Camp-컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+ import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ function Header() {
+   return <header>
+     <h1><a href="/index.html">WEB</a></h1>
+   </header>
+ }
 
-export default App;
+ function Article()
+ {
+   return <article>
+     <h2>Welcome, WEB</h2>
+   </article>
+ }
+
+ function Nav()
+ {
+   return <nav>
+     <ol>
+       <li><a href="read/1.html">HTML</a></li>
+       <li><a href="read/2.html">CSS</a></li>
+       <li><a href="read/3.html">JavaScript</a></li>
+     </ol>
+   </nav>
+ }
+
+
+ function App() {
+   return (
+     <div className="App">
+       <Header></Header>
+       <Nav></Nav>
+       <Article></Article>
+     </div>
+   );
+ }
+
+  
+ export default App;
