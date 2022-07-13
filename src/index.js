7903783519f13ebcaabf72dc0e27d383b29ad6e4
soo <soo@sooui-MacBookPro.local>
2022-07-13 11:14:02 +0900

    BrowserRouter 생성

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import{BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //index.html에서 본 root 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    {/*  <App /> </App> 이걸 한 줄로 표현 */}
+    <App /> 
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
