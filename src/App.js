513e69241ca18fb6a4373228607894d5e3fd5f10
soo <soo@sooui-MacBookPro.local>
2022-07-13 14:11:57 +0900

    onCreate로

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 import {Link,Routes,Route,useParams} from 'react-router-dom';
@@ -34,7 +34,12 @@ function Read(props){
 function Create(){
   return <article>
     <h1>Create</h1>
-    <form action="/api/create">
+    <form action="/api/create" onSubmit={evt=>{
+      evt.preventDefault();
+      const title = evt.target.title.value;
+      const body = evt.target.body.value;
+      console.log('title', title, body);
+    }}>
       <p><input type="text" name="title" placeholder="제목" /></p>
       <p><textarea name="body" placeholder="본문"></textarea></p>
       <p><input type="submit" value="생성"></input></p>
@@ -55,7 +60,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Article title="Welcome" body="Hello, WEB"></Article>}></Route>
           <Route path="/read/:id" element={<Read data={topics}></Read>}></Route>
-          <Route path="/create" element={<Create></Create>}></Route>
+          <Route path="/create" element={<Create onCreate={(title, body)=>{
+            // topics를 변경
+          }}></Create>}></Route>
         </Routes>
         <Link to="/create">create</Link>
     </div>
