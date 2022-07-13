54663e5869dae665bf7071d79aa270c6a9121262
soo <soo@sooui-MacBookPro.local>
2022-07-13 10:41:56 +0900

    누를때마다 해당 내용바꾸기

--- src/App.js
+++ src/App.js
@@ -1,42 +1,88 @@
-//import logo from './logo.svg';
- import './App.css';
- //웹사이트는 한번로딩하고 끝나는데, 이걸 동적으로 만들어서 Interative하게
- import {useState} from 'react';
- 
 
- // 이건 유사 자바스크립트로 jsx이다!
- function Header(props) {
-   return <header>
-     <h1><a href="/index.html" onClick={(event) => {
-       event.preventDefault();
-       props.onSelect();
-     }}>WEB
-       </a></h1>
-   </header>
- }
+ 1
+ 2
+ 3
+ 4
+ 5
+ 6
+ 7
+ 8
+ 9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
 
+function Header(props){
+  return <header>
+    <h1><a href="/index.html" onClick={(event)=>{
+      event.preventDefault();
+      props.onSelect();
+    }}>WEB</a></h1>
+  </header>
+}
 function Nav(props){
-  function callback(el){
-    return <li key={el.id}>
-      <a href={'/read/'+el.id} onClick={event=>{
-        event.preventDefault();
-        props.onSelect();
-      }}>
-        {el.title}
-      </a>
-  </li>
-  }
   return <nav>
     <ol>
-      {/* {props.data.map(el => <li key={el.id}>
+      {props.data.map(el => <li key={el.id}>
         <a href={'/read/'+el.id} onClick={event=>{
           event.preventDefault();
-          props.onSelect();
+          props.onSelect(el.id);
         }}>
           {el.title}
         </a>
-      </li>)} */}
-      {props.data.map(callback)}
+      </li>)}
     </ol>
   </nav>
 }
@@ -48,6 +94,7 @@ function Article(props) {
 }
 function App() {
   const [mode, setMode] = useState('READ');
+  const [id, setId] = useState(null);
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
@@ -57,19 +104,22 @@ function App() {
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB"></Article>
   } else if(mode === 'READ'){
-    content = <Article title="READ" body="Hello, READ"></Article>
+    const topic = topics.filter(el=>el.id === id)[0];
+    console.log('topic', topic);
+    //content = <Article title="READ" body="Hello, READ"></Article>
+    //개개인별로
+    content = <Article title={totpic.title} body={totpic.body}></Article>
   }
   return (
-    <div style={{
-      border:'1px solid red'
-    }}>
+    <div>
         <Header onSelect={()=>setMode('WELCOME')}></Header>
-        <Nav data={topics}  onSelect={()=>setMode('READ')}></Nav>
+        <Nav data={topics}  onSelect={(_id)=>{
+          setMode('READ');
+          setId(_id);
+        }}></Nav>
         {content}
     </div>
   );
 }
+
 export default App;
-
-
-
