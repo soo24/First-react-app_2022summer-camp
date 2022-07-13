5d0883ad8c7bf238ef24d37aefb4598765724c1c
soo <soo@sooui-MacBookPro.local>
2022-07-13 11:31:37 +0900

    필수적인것만 남기고, 나머지 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
-
-//url 만 쑥 바꾸고 싶어!
-import {Link, Routes,Route} from 'react-router-dom';
+import {Link,Routes,Route} from 'react-router-dom';
 
 function Header(props){
   return <header>
-    <h1><Link to='/' onClick={(event)=>{
-      event.preventDefault();
-      props.onSelect();
-    }}>WEB</Link></h1>
+    <h1><Link to="/">WEB</Link></h1>
   </header>
 }
 function Nav(props){
   return <nav>
     <ol>
       {props.data.map(el => <li key={el.id}>
-        <Link to={'/read/'+el.id} onClick={event=>{
-          // event.preventDefault();
-          // props.onSelect(el.id);
-        }}>
+        <Link to={'/read/'+el.id}>
           {el.title}
         </Link>
       </li>)}
@@ -34,37 +26,21 @@ function Article(props) {
   </article>;
 }
 function App() {
-  const [mode, setMode] = useState('WELCOME');
-  const [id, setId] = useState(null);
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'js', body:'js is ...'}
   ]
-  let content = null;
-  if(mode === 'WELCOME'){
-    content = <Article title="Welcome" body="Hello, WEB"></Article>
-  } else if(mode === 'READ'){
-    const topic = topics.filter(el=>el.id === id)[0];
-    content = <Article title={topic.title} body={topic.body}></Article>
-  }
+  
   return (
     <div>
-        <Header onSelect={()=>setMode('WELCOME')}></Header>
-        <Nav data={topics}  onSelect={(_id)=>{
-          setMode('READ');
-          setId(_id);
-        }}></Nav>
-        {/* 조건문이 없어지는게 아니라 우리 눈에서만 사라지게 */}
-        {/* {content} */}
-
+        <Header></Header>
+        <Nav data={topics}></Nav>
         <Routes>
-          <Route path="/" element= {<Article title="Welcome" body="Hello, WEB"></Article>}></Route>
-          <Route path="/read/:id" element= {<Article title="Read" body="Hello, WEB"></Article>}></Route>
-        {/* :id 는 wild 하게 된다, :id는 가변적이다! */}
+          <Route path="/" element={<Article title="Welcome" body="Hello, WEB"></Article>}></Route>
+          <Route path="/read/:id" element={<Article title="Read" body="Hello, READ"></Article>}></Route>
         </Routes>
     </div>
   );
 }
-
 export default App;
